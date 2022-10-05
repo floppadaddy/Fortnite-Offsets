cef5bb68f495e339cc8ccbf0d0ebd60ea7621ebd
floppadaddy <112397942+floppadaddy@users.noreply.github.com>
2022-10-05 18:45:03 +0300

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,43 @@
+fname pool: 0xDF42880
+
+Uworld: 0xDF0FE00
+
+Gameinstance: 0x1a8
+
+TeamIndex: 0x1098
+
+Mesh: 0x310
+
+LocalPlayers: 0X38
+
+PlayerController: 0x30
+
+RootComponent: 0X190
+
+AcknowledgedPawn: 0x328
+
+otherPlayerState: 0x2a8
+
+bonearray: 0x5a0
+
+ReviveFromDBNOTime: 0x4170
+
+PlayerState: 0x2a8
+
+PersistentLevel: 0X30
+
+ActorCountChangedDelegate: 0xA0
+
+ArrayProperty: 0x8
+
+RelativeLocation: 0X128
+
+CurrentWeapon: 0x858
+
+WeaponData: 0x3F0
+
+bIsaBot: 0x292
+
+Rarity: 0x70
+
+bAlreadySearched: 01039
